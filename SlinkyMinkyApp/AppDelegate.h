b9a048a4c00c4fa320ea0f2133b6f365bd6448e3
KaushiKularatnam <kaushalya.kularatnam@lbi.com>
2012-03-12 12:23:42 +0000

    Initial Commit

---
+++ SlinkyMinkyApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlinkyMinkyApp
+//
+//  Created by Kaushalya Kularatnam on 12/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlinkyMinkyApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlinkyMinkyApp
+//
+//  Created by Kaushalya Kularatnam on 12/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
